523e7646f1c54ce79b78682d8e3410589ec6f080
arthur-glg <arthur.guillot-le-goff@etu.utc.fr>
2021-11-22 17:21:06 +0100

    first

---
+++ data/data_1.js
@@ -0,0 +1,8 @@
+var json_data_1 = {
+"type": "FeatureCollection",
+"name": "data_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Honga Power Plant ", "X": 19.691483981842431, "Y": 101.28016057071477, "photo": "HongaPowerPlant.jpg", "desription": "Hongsa power plant project is a backbone project under the framework agreement for the energy cooperation between Thailand and Laos. The part of it undertaken by CNEEC involved a total investment of about $1.68 billion" }, "geometry": { "type": "Point", "coordinates": [ 101.28016057071477, 19.691483981842431 ] } }
+]
+}
